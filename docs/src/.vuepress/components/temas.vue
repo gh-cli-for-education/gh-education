546bcb0816ccf174e48044cdbac84066bc2ebc5b
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-02-14 12:17:51 +0000

    2122

--- docs/src/.vuepress/components/temas.vue
+++ docs/src/.vuepress/components/temas.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <ul>
-            <li><a href="/temas/introduccion-a-pl">Introducción a PL</a></li>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <li><a href="/temas/introduccion-a-pl">Introducción a PL</a></li>
+      <li><a href="https://docs.google.com/presentation/d/1N8h99dXzud9HzH8XY6QCZSmATCAWXtZebuqRTiy8qMU/edit?usp=sharing">Introducción to Compilers</a></li>
+      <li><a href="/temas/introduccion-a-pl/esprima.html">First Examples with Espree</a></li>
+    </ul>
+  </div>
 </template>
