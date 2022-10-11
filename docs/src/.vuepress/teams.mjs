34a266ccb7ef9f8c99fc9f7994720f12f7d6252a
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-10-11 12:31:31 +0100

    2223

--- docs/src/.vuepress/teams.mjs
+++ docs/src/.vuepress/teams.mjs
@@ -1,3 +1,30 @@
+/* graphql query for the team members 
+
+query getInfo($organization: String!) {
+  organization(login: $organization) {
+    teams(first: 100) {
+      totalCount
+      edges {
+        node {
+          name
+          members(first: 100) {
+            totalCount
+            edges {
+              memberAccessUrl
+              node {
+                name
+                url
+                email                
+              }
+            }
+          }
+          url
+        }
+      }
+    }
+  }
+}
+*/
 export default {
   "data": {
     "organization": {
