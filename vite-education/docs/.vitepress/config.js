344ca26df0e6d2ff7ec5271c056a0a0246e719a7
Carlos Díaz <alu0101102726@ull.edu.es>
2022-11-02 20:11:13 +0000

    Añadida estructura inicial

---
+++ vite-education/docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'GH EDUCATION', // appended to all page titles
+    themeConfig: {
+        nav: [
+            { text: '🏠 Inicio', link: '/' },
+            { text: '🕒 Horario', link: '/horario/horario' },
+            { text: '🧑🏽‍🏫 Clases', link: '/clases/clases' },
+            { text: '💻 Prácticas', link: '/practicas/practicas' },
+            { text: '👥 Teams', link: '/teams/teams' },
+            { text: '📝 Temas', link: '/temas/temas' },
+        ]
+    }
+}
--- vite-education/docs/index.md
+++ vite-education/docs/index.md
@@ -1,2 +1,1 @@
-# Hello VitePress
-## Curso
+# Página principal
