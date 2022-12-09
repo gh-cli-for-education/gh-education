1d2a2796699b1b050ec908d2e1152b102144aae1
Carlos Díaz <alu0101102726@ull.edu.es>
2022-12-09 12:14:37 +0000

    configuration firebase auth

--- vite-education/docs/.vitepress/config.js
+++ vite-education/docs/.vitepress/config.js
@@ -13,6 +13,61 @@ module.exports = {
     markdown: {
         lineNumbers: true,
       },
+    head: [
+        [
+            "script",
+            {
+              src: "https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js"
+            }
+          ],
+      
+          [
+            "script",
+            {
+              src: "https://www.gstatic.com/firebasejs/5.5.6/firebase-auth.js"
+            }
+          ],
+      
+          [
+            "script",
+            {
+              src: "https://www.gstatic.com/firebasejs/5.5.6/firebase-firestore.js"
+            }
+          ],
+      
+          [
+            "script",
+            {
+              src: "https://www.gstatic.com/firebasejs/5.5.6/firebase-functions.js"
+            }
+          ],
+      
+          [
+            "script",
+            {
+              src: "https://www.gstatic.com/firebasejs/5.5.6/firebase-analytics.js"
+            }
+          ],
+
+          [
+            "script",
+            {},
+            `const firebaseConfig = {
+                apiKey: "AIzaSyC1WfyhIqmL1n7ahPL-XVSAmBECWxMTUrg",
+                authDomain: "gh-education.firebaseapp.com",
+                projectId: "gh-education",
+                storageBucket: "gh-education.appspot.com",
+                messagingSenderId: "232266340667",
+                appId: "1:232266340667:web:15dc478ab3ef65c66c7f1e",
+                measurementId: "G-1N1LRJY9RP"
+              };
+              
+              // Initialize Firebase
+              const app = initializeApp(firebaseConfig);
+              const analytics = getAnalytics(app);`
+          ],
+
+    ],
     extendMarkdown: (md) => { // See https://github.com/vuejs/vuepress/issues/222#issuecomment-874001675
     // use more markdown-it plugins!
         md.use(
