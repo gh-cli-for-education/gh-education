d1dcf4464dd9f12cc795231e2f9145ef69c14c47
Carlos Díaz Calzadilla <alu0101102726@ull.edu.es>
2023-01-09 11:58:11 +0000

    Creating update units, taks

--- vite-education/docs/.vitepress/config.js
+++ vite-education/docs/.vitepress/config.js
@@ -28,6 +28,14 @@ module.exports = {
             md.use(require('markdown-it-include'))
         }
     },
+    chainWebpack: config => {
+        config.module
+          .rule('raw')
+          .test(/\.md$/)
+          .use('raw-loader')
+          .loader('raw-loader')
+          .end()
+    },
     footer: {
         message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
         copyright: 'Copyright © 2022-present <a href="https://github.com/alu0101102726">Carlos Díaz</a>'
