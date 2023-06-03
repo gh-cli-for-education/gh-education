fd67f0dd8eaafd07a118e9ffd7d0befe8326b654
Carlos Díaz <alu0101102726@ull.edu.es>
2023-06-03 11:29:52 +0100

    deploy

---
+++ vite-education/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:alu0101102726/gh-cli-for-education/gh-education/.git main:gh-pages
+
+cd -
