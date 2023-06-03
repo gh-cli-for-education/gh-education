329d5c48c441029d040641cc1bfba120ef74e1ca
Carlos Díaz <alu0101102726@ull.edu.es>
2023-06-03 11:31:34 +0100

    deploy

--- vite-education/deploy.sh
+++ vite-education/deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gh-cli-for-education/gh-education/.git main:gh-pages
+git push -f main gh-pages
 
 cd -
